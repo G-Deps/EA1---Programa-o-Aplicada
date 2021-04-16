624cb7cb4e75f8fa4a3a547f1aea8bbb0b2f28ae
GusDeps <>
2021-04-16 01:28:34 +0000

    Código Inicial

--- main.cpp
+++ main.cpp
@@ -1,5 +1,60 @@
-#include <iostream>
+#include<iostream>
+#include<string>
 
-int main() {
-  std::cout << "Hello World!\n";
+using namespace std;
+string aux_comandos, comandos;
+
+string Decod (string _aux);
+string Tecla (int _tecla, const char _char);
+
+int main () {
+    int quantidade;
+    string resultado;
+    cout<< "Quantidade de Comandos: \n";
+    cin>>quantidade;
+    for (int i=0; i<quantidade; i++){
+        cin >> aux_comandos;
+        comandos += aux_comandos;
+        aux_comandos = "";
+    }
+    resultado = Decod(comandos);
+    cout << resultado;
+
+
+    
+}
+
+string Decod (string _aux){
+    string Cod;
+    for(int i = 0; i < signed(_aux.length());i++){
+        if(_aux[i] == '#'){        
+            switch (_aux[i+1])
+            {
+            case '2':
+                Cod += Tecla(2,_aux[i+3]);
+                break;
+            
+            default:
+                break;
+            }
+
+        }
+
+    }
+    return Cod;
+}
+
+string Tecla (int _tecla, const char _char){
+    string Carac;
+    switch (_tecla)
+    {
+    case 2:
+        if(_char == '1') 
+        Carac += "a";
+        break;
+    
+    default:
+        break;
+    }
+    return Carac;
 }
